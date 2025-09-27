1075699c6b316db1eaf01ae3925a1354074849d5
Niket Markana <niketmarkanartj6@gmail.com>
2025-09-27 09:10:04 +0530

    lec 8

---
+++ lec 07/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        // console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance}`);
+        //atleast show output of this console.log()
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+        // https://nodejs.org/api/process.html
+    }
+}
+
+export default connectDB
